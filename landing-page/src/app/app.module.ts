5352fa9e58dcea8e4876aaaedf2c3f5b0a0094f9
LuisG <luisg@localhost.localdomain>
2020-06-10 22:05:10 -0500

    feat(lending-page): added the lending page project

--- landing-page/src/app/app.module.ts
+++ landing-page/src/app/app.module.ts
@@ -3,15 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { MaterialModule } from './material/material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    NavBarComponent,
+    FooterComponent,
+    FormularioComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    
   ],
+  entryComponents: [FormularioComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
